3ab705072deb5559485061466f3156dc4cdc11bd
rabia <rabianuzha@gmail.com>
2024-05-07 11:11:13 +0100

    Temp

---
+++ Assignment 4/TempConverter/Conversion.js
@@ -0,0 +1,18 @@
+var celsius=35;
+
+var farenheit;
+//farenheit conversion
+document.write("<h4> Celcius to Farenheit conversion</h4>")
+
+document.write("Value of celsius &nbsp:&nbsp",celsius);
+farenheit=(celsius*1.8)+32;
+document.write("<br><br><b>Farenheit</b> Conversion &nbsp:&nbsp",farenheit);
+
+document.write("<h4>Farenheit to celcius conversion</h4>")
+
+// celsius conversion
+var far=46.6;
+var cels;
+document.write("Value of farenheit &nbsp: &nbsp",far);
+cels=(far-32)/1.8;
+document.write("<br><br><b>Celsius</b> Conversion &nbsp:&nbsp",cels);
